42b77251629de81f7fe965b17ab7913437618a53
jaymehta <jaysmehta91@gmail.com@example.com>
2020-08-14 11:26:47 +0530

    Initial Commit

---
+++ PTNotificationContent/PTNotificationContent.h
@@ -0,0 +1,19 @@
+//
+//  PTNotificationContent.h
+//  PTNotificationContent
+//
+//  Created by Jay Mehta on 14/08/20.
+//  Copyright © 2020 Jay Mehta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PTNotificationContent.
+FOUNDATION_EXPORT double PTNotificationContentVersionNumber;
+
+//! Project version string for PTNotificationContent.
+FOUNDATION_EXPORT const unsigned char PTNotificationContentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PTNotificationContent/PublicHeader.h>
+
+
